41649601196c550bde134e7ab7b67cc0010fc35d
Elroy Kanye <elroykanye@gmail.com>
2021-06-28 07:42:26 +0100

    reading from a stream using fgetc()

---
+++ e3-buffered-io/s1-standard-io/t4-reading-stream.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "../../apue.h"
+
+// int fgetc(FILE *stream)
+
+/*
+This function reads the next character from the stream and returns it as an unsigned character cast to an int.
+The casting is done to have a sufficient range for notification of end-of-file or error: EOF is returned in such conditions.
+The return value of fgetc() must be stored in an int. Storing it in char is a common but dangerous mistake.
+*/
+
+int main() {
+    FILE *fp;
+    int c;
+    printf("EOF = %d\n", EOF);
+
+    fp = fopen("open.txt", "r");
+    if (!fp) {
+        char *er = strerror(errno);
+        printf("%s\n", er);
+    } else {
+        c = fgetc(fp);
+        if (c == EOF) {
+            char *er = strerror(errno);
+            printf("%s\n", er);
+            exit(0);
+        } else {
+            printf("%d = %c\n", c, (char) c);
+        }
+    }
+}
