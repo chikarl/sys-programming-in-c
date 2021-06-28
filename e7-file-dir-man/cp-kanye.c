636bf3d4e0ca7143c381969a552b0a7796025e74
Elroy Kanye <elroykanye@gmail.com>
2021-06-28 06:50:46 +0100

    completed first logical operation

--- e7-file-dir-man/cp-kanye.c
+++ e7-file-dir-man/cp-kanye.c
@@ -4,15 +4,25 @@
 int main(int argc, char *argv[]) {
     char *source = argv[1];
     char *dest = argv[2];
+    char line[MAXLINE];
 
-    int src_fd, dest_fd;
+    int src_fd, dest_fd, nb;
 
     src_fd = open(source, O_RDONLY);
     if (src_fd < 0) {
         char *er = strerror(errno);
         printf("%s\n", er);
     } else {
-        dest_fd = open(dest, O_RDWR | O_CREAT | O_TRUNC);
-
+        dest_fd = creat(dest, 0644);
+        if (dest_fd < 0) {
+            char *er = strerror(errno);
+            printf("%s\n", er);
+        } else {
+            nb = read(src_fd, line, MAXLINE);
+            write(dest_fd, line, nb);
+            printf("Success");
+        }
+        close(dest_fd);
     }
+    close(src_fd);
 }
